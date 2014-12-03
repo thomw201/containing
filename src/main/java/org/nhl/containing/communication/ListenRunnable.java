9b029275894414c715366bfaf4aeb3c038a08d20
Rubykuby <rubykuby@gmail.com>
2014-12-03 11:03:58 +0100

    Revert "Also added a sleep for performance reasons"
    This reverts commit 52172e847293a0f3a81260d9f56580878cd57144.

--- src/main/java/org/nhl/containing/communication/ListenRunnable.java
+++ src/main/java/org/nhl/containing/communication/ListenRunnable.java
@@ -31,11 +31,6 @@ public class ListenRunnable implements Runnable {
                 // Write to queue
                 queue.add(inputLine);
                 System.out.println("Received " + inputLine);
-                try {
-                    Thread.sleep(250);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
             }
         } catch (IOException e) {
         }
