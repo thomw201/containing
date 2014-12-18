2ede89acdf12ec65ad7fe8a1e0084bafa83a5a3a
Jeroen van der Meulen <vandermeulen.jeroen@gmail.com>
2014-12-18 10:51:25 +0100

    All cranes now have their own list with locations for containers!

--- src/main/java/org/nhl/containing/areas/StorageArea.java
+++ src/main/java/org/nhl/containing/areas/StorageArea.java
@@ -1,6 +1,7 @@
 package org.nhl.containing.areas;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 import org.nhl.containing.cranes.StorageCrane;
 
@@ -8,18 +9,22 @@ import java.util.ArrayList;
 import java.util.List;
 import org.nhl.containing.Container;
 
-/**
- * @author Jeroen
- */
 public class StorageArea extends Area {
 
     private List<StorageCrane> storageCranes = new ArrayList();
     private List<Container> containers = new ArrayList();
+    private List<List<Vector3f>> storageLanes = new ArrayList();
     private AssetManager assetManager;
     private int craneXAxis = 0;
     private float craneRailsXAxis = 2.5f;
     private int craneRailsZAxis = 0;
     private int cranes;
+    private float loc = -12;
+    private float x;
+    private float y = 0;
+    private float z = 20;
+    private int n;
+    private int containerCounter = 0;
 
     public StorageArea(AssetManager assetManager, int cranes) {
         this.assetManager = assetManager;
@@ -53,18 +58,52 @@ public class StorageArea extends Area {
             craneRailsZAxis = 0;
             craneRailsXAxis += 50;
         }
+        
+        // Create for every crane a list with vectors for containers.
+        for (int i = 0; i < cranes; i++) {
+            n = i;
+            List<Vector3f> containerSpots = new ArrayList();
+            x = getStorageCranes().get(i).getLocalTranslation().x - 2.5f + loc;
+            for (int j = 0; j < 900; j++) {
+                containerSpots.add(getNextSpot());
+            }
+            storageLanes.add(containerSpots);
+            y = 0;
+            z = 20;
+        }
 
     }
-    
-    public List<StorageCrane> getStorageCranes(){
+
+    // Get next location for the containers.
+    public Vector3f getNextSpot() {
+        x += 2.5f;
+        if (x >= getStorageCranes().get(n).getLocalTranslation().x + 25f + loc) { 
+            x = getStorageCranes().get(n).getLocalTranslation().x + loc;
+            z += 13.5f;
+        }
+        if (z >= 222.5f) {
+            x = getStorageCranes().get(n).getLocalTranslation().x + loc;
+            z = 20;
+            y += 2.9f;
+        }
+        
+        containerCounter++;
+        return new Vector3f(x, y, z);
+    }
+
+    public List<StorageCrane> getStorageCranes() {
         return storageCranes;
     }
     
-    public void addContainer(Container container){
+    public List<List<Vector3f>> getStorageLanes(){
+        return storageLanes;
+    }
+
+    public void addContainer(Container container) {
         containers.add(container);
     }
-    
-    public void removeContainer(Container container){
+
+    public void removeContainer(Container container) {
         containers.remove(container);
     }
 }
